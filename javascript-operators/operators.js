5c7e166c06fba5eabc1605dd81b8895f19285e95
sourabh singh <sourabhruhal0999@gmail.com>
2022-11-08 18:35:49 +0000

    javascript operators file is added

---
+++ javascript-operators/operators.js
@@ -0,0 +1,29 @@
+//We learn about the different operators used in js 
+
+//(1)-> '+' operator : This operator have two task one for concatenation and second for general addding 
+
+//This is concatenation 
+var firstName = 'sourabh';
+var lastName = 'Singh';
+console.log(firstName + lastName);
+
+//for general adding 
+var a = 10109;
+var b = 201;
+console.log(a +b );
+
+//(2)-> '-' operator : 
+console.log(a-b);
+
+//(3)-> '*' operator:
+console.log(a*b);
+
+//(4)-> '/' operator : for division 
+var d = a/b //50.29353233833084
+//now in order to make it more precision we can round of it 
+console.log((d).toFixed(2));  //50.29
+
+//(5)-> '%' operator : 
+console.log(a%b);
+
+
